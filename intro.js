13f39e95aa31a6d24ad0f1fd39eb0bbc7c463527
Kurt Bauer <kurtbauer55@protonmail.com>
2019-01-04 14:12:22 -0500

    adding container property to styles

--- intro.js
+++ intro.js
@@ -2,5 +2,10 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
-    
+    container: {
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+        flex: 1,
+        justifyContent: 'center',
+    } 
 });
